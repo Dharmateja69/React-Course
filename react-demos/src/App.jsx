240f9d2fb237113928b9db732d1164b04fa442e6
Dharmateja69 <ddharmateja69@gmail.com>
2025-02-10 00:23:06 +0530

    Conditional rendering

--- react-demos/src/App.jsx
+++ react-demos/src/App.jsx
@@ -1,37 +1,15 @@
 import React from "react";
-import Cards from "./Components/Cards";
 
+const ValidPassword = () => <h1>Valid Password</h1>;
+const InvalidPassword = () => <h1>InValid Password</h1>;
+
+const Password = ({ isvalid }) => {
+  return isvalid ? <ValidPassword /> : <InvalidPassword />;
+};
 export default function App() {
   return (
-    //As we are passing the body of the card unless the property...we will accept this in the children keyword in child component.
     <div>
-      <Cards>
-        <h1>this is the card heading!!</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta
-          deserunt delectus officiis, ex veritatis, nam molestias itaque
-          accusamus sapiente aspernatur similique libero iusto! Dicta dolor,
-          fugit aperiam illo accusantium asperiores.
-        </p>
-      </Cards>
-      <Cards>
-        <h1>this is the card heading1!!</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta
-          deserunt delectus officiis, ex veritatis, nam molestias itaque
-          accusamus sapiente aspernatur similique libero iusto! Dicta dolor,
-          fugit aperiam illo accusantium asperiores.
-        </p>
-      </Cards>
-      <Cards>
-        <h1>this is the card heading2!!</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta
-          deserunt delectus officiis, ex veritatis, nam molestias itaque
-          accusamus sapiente aspernatur similique libero iusto! Dicta dolor,
-          fugit aperiam illo accusantium asperiores.
-        </p>
-      </Cards>
+      <Password isvalid={true} />
     </div>
   );
 }
