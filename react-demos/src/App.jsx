7853e069ae27a3a4e0e8974323fc0e8ce446753a
Dharmateja69 <ddharmateja69@gmail.com>
2025-02-09 23:58:17 +0530

    children component

--- react-demos/src/App.jsx
+++ react-demos/src/App.jsx
@@ -1,12 +1,37 @@
 import React from "react";
-import Person from "./Assignment/Person";
-import Product from "./Assignment/Product";
+import Cards from "./Components/Cards";
 
 export default function App() {
   return (
+    //As we are passing the body of the card unless the property...we will accept this in the children keyword in child component.
     <div>
-      <Product name="rexona" price="25" />
-      <Person name="sidhu" age="23" />
+      <Cards>
+        <h1>this is the card heading!!</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta
+          deserunt delectus officiis, ex veritatis, nam molestias itaque
+          accusamus sapiente aspernatur similique libero iusto! Dicta dolor,
+          fugit aperiam illo accusantium asperiores.
+        </p>
+      </Cards>
+      <Cards>
+        <h1>this is the card heading1!!</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta
+          deserunt delectus officiis, ex veritatis, nam molestias itaque
+          accusamus sapiente aspernatur similique libero iusto! Dicta dolor,
+          fugit aperiam illo accusantium asperiores.
+        </p>
+      </Cards>
+      <Cards>
+        <h1>this is the card heading2!!</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta
+          deserunt delectus officiis, ex veritatis, nam molestias itaque
+          accusamus sapiente aspernatur similique libero iusto! Dicta dolor,
+          fugit aperiam illo accusantium asperiores.
+        </p>
+      </Cards>
     </div>
   );
 }
