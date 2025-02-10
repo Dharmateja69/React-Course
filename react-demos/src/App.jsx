a75dd522653a9b25eaff30dcd26a071c0c42c64b
Dharmateja69 <ddharmateja69@gmail.com>
2025-02-11 00:12:03 +0530

    Event

--- react-demos/src/App.jsx
+++ react-demos/src/App.jsx
@@ -1,30 +1,21 @@
 import React from "react";
-import Iconcompentnt from "./Assignment/Iconcompentnt";
-import ProfileCard from "./Assignment/ProfileCard";
-import StyleCard from "./Assignment/StyleCard";
+
+const Button = () => {
+  const handleclick = () => {
+    console.log("click me ", Math.round(Math.random() * 10));
+  };
+
+  return (
+    <h1>
+      <button onClick={handleclick}>Click</button>
+    </h1>
+  );
+};
 
 export default function App() {
   return (
     <div>
-      <StyleCard>
-        <h1>Hi this is the StyleCard unit!!</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-          Necessitatibus odio similique pariatur repudiandae fuga dolorum
-          accusantium quod harum quaerat quos, placeat ex cum dolore quas?
-          Reprehenderit sunt nobis reiciendis ex?
-        </p>
-      </StyleCard>
-      <ProfileCard>
-        <h1>Hi this is the StyleCard unit!!</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-          Necessitatibus odio similique pariatur repudiandae fuga dolorum
-          accusantium quod harum quaerat quos, placeat ex cum dolore quas?
-          Reprehenderit sunt nobis reiciendis ex?
-        </p>
-      </ProfileCard>
-      <Iconcompentnt />
+      <Button />
     </div>
   );
 }
