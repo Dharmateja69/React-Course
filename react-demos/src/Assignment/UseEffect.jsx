73c26452897fea6a421db0c65bb36ba6436d9187
Dharmateja69 <ddharmateja69@gmail.com>
2025-02-16 22:57:54 +0530

    fetch data using useEffect()

--- react-demos/src/Assignment/UseEffect.jsx
+++ react-demos/src/Assignment/UseEffect.jsx
@@ -1,9 +1,34 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const UseEffect = () => {
   const [data, setData] = useState([]);
-  useEffect(() => {}, []); //this empty {},[]) mean this method will run for one time
-  return <div></div>;
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos"
+      );
+
+      const data = await response.json();
+      if (data && data.length) {
+        setData(data);
+      }
+    }
+    getData();
+  }, []); //this empty {},[]) mean this method will run for one time.
+
+  return (
+    <div>
+      <ul>
+        {data.map((todo) => (
+          <section key={todo.id}>
+            <li>{todo.id}</li>
+
+            <li>{todo.title}</li>
+          </section>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default UseEffect;
