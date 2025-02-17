2989968239ac17cfb890dd8abdedb3fa552d7c3b
Dharmateja69 <ddharmateja69@gmail.com>
2025-02-17 19:37:00 +0530

    context api with more data

--- react-demos/src/App.jsx
+++ react-demos/src/App.jsx
@@ -2,14 +2,18 @@ import React, { createContext } from "react";
 import ComponentA from "./Components/ComponentA";
 
 export const Data = createContext();
+export const Data1 = createContext();
 
 export default function App() {
   const name = "sidhu";
+  const age = 22;
 
   return (
     <div>
       <Data.Provider value={name}>
-        <ComponentA />
+        <Data1.Provider value={age}>
+          <ComponentA />
+        </Data1.Provider>
       </Data.Provider>
     </div>
   );
--- react-demos/src/Components/ComponentC.jsx
+++ react-demos/src/Components/ComponentC.jsx
@@ -1,12 +1,23 @@
 import React from "react";
-import { Data } from "../App";
+import { Data, Data1 } from "../App";
 
 const ComponentC = () => {
   return (
     <div>
       <Data.Consumer>
         {(name) => {
-          return <h1>{name}</h1>;
+          // return <h1>{name}</h1>;
+          return (
+            <Data1.Consumer>
+              {(age) => {
+                return (
+                  <h1>
+                    My name is {name} and my age is {age}
+                  </h1>
+                );
+              }}
+            </Data1.Consumer>
+          );
         }}
       </Data.Consumer>
     </div>
@@ -14,3 +25,5 @@ const ComponentC = () => {
 };
 
 export default ComponentC;
+
+// Every time new data is added, updating the callback function becomes a problem. We can solve this issue using the useContext hook.
