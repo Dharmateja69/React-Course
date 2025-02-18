d56fcbf95d418bdf826b391ce2d947421ebe1fc6
Dharmateja69 <ddharmateja69@gmail.com>
2025-02-18 23:09:51 +0530

    Context API

--- react-demos/src/Assignment/Userdata.jsx
+++ react-demos/src/Assignment/Userdata.jsx
@@ -4,13 +4,14 @@ import { UserContext } from "./UserProvider";
 
 const Userdata = () => {
   const { user, updateUser } = useContext(UserContext);
-  const { color } = useContext(ColorContext);
+  const { color, update } = useContext(ColorContext);
   const [newname, setname] = useState("");
+  const [newcolor, setnewcolor] = useState("");
   return (
     <div>
       <h1>User Data</h1>
       <p style={{ color: color }}>Name:{user.name}</p>
-      <p style={{ color: color }}>Age:{user.age}</p>
+      <p>Age:{user.age}</p>
       <input
         type="text"
         placeholder="enter the updating name"
@@ -19,6 +20,14 @@ const Userdata = () => {
       />
       <button type="submit" onClick={() => updateUser(newname)}>
         update
+      </button>
+      <input
+        type="color"
+        value={newcolor}
+        onChange={(e) => setnewcolor(e.target.value)}
+      />
+      <button type="submit" onClick={() => update(newcolor)}>
+        Click to change the colour
       </button>
     </div>
   );
