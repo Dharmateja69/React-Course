3b5139e7fc22c745f8cefb6dc8ce905c90022c88
Dharmateja69 <ddharmateja69@gmail.com>
2025-03-17 14:26:25 +0530

    updated

--- react-demos/src/main.jsx
+++ react-demos/src/main.jsx
@@ -1,5 +1,9 @@
 import { createRoot } from "react-dom/client";
 
 import App from "./App.jsx";
-
-createRoot(document.getElementById("root")).render(<App />);
+import MyProvider from "./MyContext.jsx";
+createRoot(document.getElementById("root")).render(
+  <MyProvider>
+    <App />
+  </MyProvider>
+);
